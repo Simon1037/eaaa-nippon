70d424863b0dd23821374d49a5e906a297f3fe04
MohanadAboud <mohanad.aboud7@gmail.com>
2024-10-07 14:06:21 +0200

    nav+footer
    footer

---
+++ js/spinner.js
@@ -0,0 +1,6 @@
+"use strict";
+
+window.addEventListener("load", function () {
+  document.getElementById("index.html").style.display = "none";
+  document.getElementById("loading").style.display = "block";
+});
