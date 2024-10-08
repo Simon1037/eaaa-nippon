257445ad82e06360e85f3f472554cbe3913e44dc
MohanadAboud <mohanad.aboud7@gmail.com>
2024-10-08 11:03:33 +0200

    forside og måltidskasse side

--- js/spinner.js
+++ js/spinner.js
@@ -1,6 +1,6 @@
 "use strict";
 
-window.addEventListener("load", function () {
-  document.getElementById("index.html").style.display = "none";
-  document.getElementById("loading").style.display = "block";
-});
+// window.addEventListener("load", function () {
+//   document.getElementById("index.html").style.display = "none";
+//   document.getElementById("loading").style.display = "block";
+// });
